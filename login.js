fd7ce156aa316ec2fc5901a31e63b5f3a9a51ba1
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-02 12:13:44 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,10 +1,13 @@
+
 function submitName() {
   const nameInput = document.getElementById("player-name");
   const playerName = nameInput.value.trim();
   if (playerName) {
-    document.getElementById("player-display").innerText = `Player: ${playerName}`;
-    document.getElementById("login-block").style.display = "none";
-    // Game kan hier starten indien nodig
+    document.getElementById("player-display").innerText = `Player ${playerName}`;
+    document.getElementById("login-overlay").style.display = "none";
+    window.playerName = playerName;
+    startTime = Date.now();
+    if (typeof renderHighscores === 'function') renderHighscores();
   } else {
     alert("Voer een geldige naam in.");
   }
