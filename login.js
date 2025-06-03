22b7b0a0de4718f7f562afe154b01a8897b7435f
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-03 14:23:41 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -5,6 +5,7 @@ function submitName() {
     window.currentPlayer = playerName;
     document.getElementById("player-display").textContent = "Player: " + playerName;
     document.getElementById("login-overlay").style.display = "none";
+    // Zet klaar om spel te starten, maar start nog NIET automatisch
     window.readyToLaunch = true;
   } else {
     alert("Vul je naam in om te starten.");
