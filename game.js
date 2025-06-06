69c1ded46600c983c6546da99346207316c318d9
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-06 23:25:11 +0200

    Update game.js

--- game.js
+++ game.js
@@ -64,6 +64,18 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
+document.addEventListener("keydown", function (e) {
+  if (flagsOnPaddle && (e.code === "ArrowUp" || e.code === "ArrowLeft" || e.code === "ArrowRight" || e.code === "Space")) {
+    shootFromFlags();
+  }
+});
+
+document.addEventListener("mousedown", function () {
+  if (flagsOnPaddle) {
+    shootFromFlags();
+  }
+});
+
 function keyDownHandler(e) {
   if (e.key === "Right" || e.key === "ArrowRight") rightPressed = true;
   else if (e.key === "Left" || e.key === "ArrowLeft") leftPressed = true;
