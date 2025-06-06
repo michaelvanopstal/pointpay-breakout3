50258003fce6a826b94a410847d41f3b0aede11d
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-06 14:06:03 +0200

    Update game.js

--- game.js
+++ game.js
@@ -118,18 +118,27 @@ function collisionDetection() {
           score += 10;
           spawnCoin(b.x, b.y);
           document.getElementById("scoreDisplay").textContent = "score " + score + " pxp.";
-  if (powerBlock.active && powerBlock.visible) {
-    if (
-      x > powerBlock.x &&
-      x < powerBlock.x + powerBlock.width &&
-      y > powerBlock.y &&
-      y < powerBlock.y + powerBlock.height
-    ) {
-      dy = -dy;
-      powerBlock.active = false;
-      powerBlockHit = true;
+  if if (powerBlock.active && powerBlock.visible) {
+  if (
+    x > powerBlock.x &&
+    x < powerBlock.x + powerBlock.width &&
+    y > powerBlock.y &&
+    y < powerBlock.y + powerBlock.height
+  ) {
+    dy = -dy;
+    powerBlock.active = false;
+    powerBlockHit = true;
+
+    // Verwijder ook de brick eronder
+    if (bricks[powerBlockCol] && bricks[powerBlockCol][powerBlockRow]) {
+      bricks[powerBlockCol][powerBlockRow].status = 0;
     }
+
+    score += 10;
+    document.getElementById("scoreDisplay").textContent = "score " + score + " pxp.";
   }
+}
+
 
         }
       }
