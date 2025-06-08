8dec8eb142a590b2ec8d4a22ae89f900228f61c8
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-08 11:49:36 +0200

    Update game.js

--- game.js
+++ game.js
@@ -83,8 +83,8 @@ document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
 document.addEventListener("keydown", (e) => {
-  if ((e.key === "ArrowUp" || e.key === "Up") && !ballLaunched && window.readyToLaunch) {
-    ballLaunched = true;
+if ((e.key === "ArrowUp" || e.key === "Up") && !ballLaunched) {
+  ballLaunched = true;
     dx = 0;
     dy = -4;
     if (!timerRunning) startTimer();score = 0;
