d7c97aaf149093408ce386ab49d833b7596f3fea
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-09 10:44:22 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,8 +1,6 @@
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 648;
-canvas.height = 600;
 
 let elapsedTime = 0;
 let timerInterval = null;
